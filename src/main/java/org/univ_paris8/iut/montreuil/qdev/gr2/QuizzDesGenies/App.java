bf51ecf73ff2a5da3143010d4a9ae0cef40f5222
weDeZed <juba.chabane8@gmail.com>
2024-03-26 15:49:33 +0100

    Initial commit

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/gr2/QuizzDesGenies/App.java
@@ -0,0 +1,13 @@
+package org.univ_paris8.iut.montreuil.qdev.gr2.QuizzDesGenies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
